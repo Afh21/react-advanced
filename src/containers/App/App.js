09294787b97c050bdce6f563d61f84c30d489938
Andres Felipe H <ahoyos@lean-tech.io>
2020-05-07 07:40:29 -0500

    Added Routes in App.jsx , Css folder

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -10,6 +10,23 @@ function App () {
         <Layout >
           <Switch>
             <Route path="/" component={HomeContainer} />
+            <Route path="/css" component={HomeContainer} />
+            <Route path="/patterns" component={HomeContainer} />
+            <Route path="/hooks" component={HomeContainer} />
+            <Route path="/functions" component={HomeContainer} />
+            <Route path="/webgl" component={HomeContainer} />
+            <Route path="/webrtc" component={HomeContainer} />
+            <Route path="/html" component={HomeContainer} />
+            <Route path="/regex" component={HomeContainer} />
+            <Route path="/maps" component={HomeContainer} />
+            <Route path="/rxjs" component={HomeContainer} />
+            <Route path="/pwa" component={HomeContainer} />
+            <Route path="/functional" component={HomeContainer} />
+            <Route path="/tdd" component={HomeContainer} />
+            <Route path="/data" component={HomeContainer} />
+            <Route path="/utils" component={HomeContainer} />
+            <Route path="/phrases" component={HomeContainer} />
+            <Route path="/verbs" component={HomeContainer} />
           </Switch>
       </Layout>
     </Router>
---
+++ src/containers/Css/Css.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Css() {
+  return (
+    <div>
+      Hi Css!
+    </div>
+  )
+}
+
+export default Css
